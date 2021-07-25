d6d4546026bf71580ee840cb42a8c93ad3637187
Deivid <you@example.com>
2021-07-25 19:21:59 -0300

    variaveis

---
+++ variaveis/script.aula.js
@@ -0,0 +1,28 @@
+var nome = 'Deivid';
+var idade = 18;
+var possuiFaculdade = true
+
+console.log(nome, idade, possuiFaculdade);
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+
+console.log(totalPreco);
+
+var sobrenome = 'Trofino',
+  cidade = 'SP';
+
+console.log(sobrenome, cidade);
+
+var semDefinir;
+console.log(semDefinir);
+
+// console.log(aindaNaoDefine);
+var comida
+console.log(comida);
+comida = 'pizza';
+console.log(comida);
+
+var time = 'Bayern';
+console.log(time);
---
+++ variaveis/script.js
@@ -0,0 +1,16 @@
+// Declarar uma variável com o seu nome
+var nome = 'Deivid';
+
+// Declarar uma variável com a sua idade
+var idade = 18;
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+var comidaFavorita;
+// Atribuir valor a sua comida favorita
+comidaFavorita = 'Pizza';
+// Declarar 5 variáveis diferentes sem valores
+var time,
+  pais,
+  cidade,
+  endereco
+console.log(nome, idade, comidaFavorita, time, pais, cidade, endereco);
