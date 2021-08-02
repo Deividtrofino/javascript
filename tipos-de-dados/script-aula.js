16fa5816427271f39565738ea839222dcd9f5196
Deivid <you@example.com>
2021-08-02 15:36:31 -0300

    tipos-de-dados.

---
+++ tipos-de-dados/script-aula.js
@@ -0,0 +1,25 @@
+var nome = 'Deivid';
+var idade = 18;
+var simbolo = Symbol();
+console.log(typeof simbolo)
+
+var nome = 'Deivid';
+var sobrenome = 'Trofino';
+var nomeCompleto = nome + ' ' + sobrenome;
+console.log(nomeCompleto);
+
+//var gols = 1000;
+//var frase = 'Romário fez ' + gols + ' gols';
+//console.log(typeof frase);
+
+var ano = '2020';
+var mes = 8;
+console.log(ano + mes);
+
+var melhor = 'teste';
+var frase1 = "Esse é o \"melhor\" jogo";
+console.log(frase1);
+
+var gols = 1000;
+var frase2 = `Romário fez ${gols} gols`;
+console.log(frase2);
---
+++ tipos-de-dados/script.js
@@ -0,0 +1,18 @@
+// Declare uma variável contendo uma string
+var nome = 'Deivid';
+
+// Declare uma variável contendo um número dentro de uma string
+var ano = '2020';
+
+// Declare uma variável com a sua idade
+var idade = 18
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+var sobrenome = 'Trofino'
+var nomeCompleto = `${nome} ${sobrenome}`;
+
+// Coloque a seguinte frase em uma variável: It's time
+var frase = "it's time";
+// Verifique o tipo da variável que contém o seu nome
+var verificarTipoNome = typeof nome;
+console.log(verificarTipoNome);
